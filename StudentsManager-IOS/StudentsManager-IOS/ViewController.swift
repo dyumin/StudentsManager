769d7b674a848fa11e01b843644cdc88487535e6
Alexey Dyumin <jejni.j@gmail.com>
2018-09-12 12:42:11 +0300

    Added simple project with dlib dependency

---
+++ StudentsManager-IOS/StudentsManager-IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudentsManager-IOS
+//
+//  Created by Дюмин Алексей on 12/09/2018.
+//  Copyright © 2018 Bauman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
