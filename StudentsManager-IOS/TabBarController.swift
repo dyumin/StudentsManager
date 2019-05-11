887d7fc091086aa01380dea4a384d1eb125c8c23
Alexey Dyumin <jejni.j@gmail.com>
2019-05-11 21:59:32 +0300

    Disable SessionPhotos if there are no selectedSession

--- StudentsManager-IOS/TabBarController.swift
+++ StudentsManager-IOS/TabBarController.swift
@@ -10,21 +10,37 @@ import UIKit
 
 import RxSwift
 
+import Firebase
+
 class TabBarController: UITabBarController
 {
 //    var initialViewControllers: [UIViewController]?
     
-//    var disposeBag: DisposeBag?
+    var disposeBag: DisposeBag?
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
+        let disposeBag = DisposeBag()
+        
+        Api.sharedApi.user.map(
+        { (documentSnapshot) -> Bool in
+            
+            return documentSnapshot?.get(ApiUser.selectedSession) as? DocumentReference != nil
+            
+        }).debug("CurrentSession.selectedSession").subscribe(
+            onNext: { [weak self] event in
+            
+            self?.viewControllers?.first?.tabBarItem.isEnabled = event
+            
+        }).disposed(by: disposeBag)
+        
+        self.disposeBag = disposeBag
+        
 //        if initialViewControllers == nil {
 //            initialViewControllers = viewControllers
 //        }
-//
-//        let disposeBag = DisposeBag()
 //
 //        Api.sharedApi.editingAllowed.distinctUntilChanged().subscribe(
 //        onNext:{ [weak self] (editingAllowed) in
