7355da482c28379ecd9be490a9844adb2fe2f855
Alexey Dyumin <jejni.j@gmail.com>
2019-05-13 17:50:25 +0300

    Add photo upload

--- StudentsManager-IOS/SessionPhotos/SessionPhotos.swift
+++ StudentsManager-IOS/SessionPhotos/SessionPhotos.swift
@@ -17,6 +17,8 @@ import RxCocoa
 
 import Firebase
 
+import RxFirebaseStorage
+
 class SessionPhotos: UIViewController
 {
     private var disposeBag = DisposeBag()
@@ -67,7 +69,15 @@ class SessionPhotos: UIViewController
             .map { info in
                 return info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage
             }
-            .debug().subscribe()
+            .debug().subscribe(
+            onNext: { [weak self] image in
+                
+                if let image = image
+                {
+                    self?.photoTaken(photo: image)
+                }
+                
+            })
             .disposed(by: disposeBag)
         
         Api.sharedApi.editingAllowed.distinctUntilChanged().map
@@ -97,10 +107,63 @@ class SessionPhotos: UIViewController
         // Do any additional setup after loading the view.
     }
     
-//    func photoTaken(photo: UIImage)
-//    {
-//        <#function body#>
-//    }
+    func photoTaken(photo: UIImage)
+    {
+        guard let selectedSession = Api.sharedApi.selectedSession.value else { return }
+        
+        // TODO: think about disposable
+        _ = selectedSession.rx.getDocument().subscribe(onNext:
+        { selectedSessionSnapshot in
+            
+            guard selectedSessionSnapshot.exists else { return }
+            
+            let storage = Storage.storage()
+            
+            let name = "\(UUID().uuidString)"
+            let imageName = "\(name).jpg"
+            
+            let path = "/sessions/\(selectedSession.documentID)/media/\(imageName)"
+            
+            guard let jpegData = photo.jpegData(compressionQuality: 1) else { return }
+            
+            // TODO: think about disposable
+            storage.reference(withPath: path).rx.putData(jpegData).subscribe(
+            onNext:
+            { metadata in
+                print(metadata)
+                
+                let db = Firestore.firestore()
+                
+                let batch = db.batch()
+                
+                let resourceRecordData = [ ResourceRecord.name : imageName,
+                                           ResourceRecord.processed : false] as [String : Any]
+                let resourceRecordRef = selectedSession.collection(Session.resources).document()
+                
+                let processingQueueRecordData = [ ProcessingQueue.imageMeta : resourceRecordRef,
+                                                  ProcessingQueue.imagePath : path,
+                                                  ProcessingQueue.session : selectedSession,
+                                                  ProcessingQueue.active : false,
+                                                  ProcessingQueue.lastUpdateTime : Timestamp.init() ] as [String : Any]
+                let processingQueueRecordRef = db.collection("processingQueue").document()
+                
+                batch.setData(resourceRecordData, forDocument: resourceRecordRef)
+                batch.setData(processingQueueRecordData, forDocument: processingQueueRecordRef)
+                
+                // TODO: think about disposable
+                _ = batch.rx.commit().subscribe(
+                onNext: { error in
+                    print(error)
+                    assertionFailure()
+                })
+            },
+            onError:
+            { error in
+                print(error)
+                assertionFailure()
+            })
+        })
+    }
     
 
     /*
